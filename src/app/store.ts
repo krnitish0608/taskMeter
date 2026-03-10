5b9ba0cc473630cab3802d3883af6580c1655cb2
krnitish0608 <krnitish0608@gmail.com>
2026-03-10 23:56:46 +0530

    store

---
+++ src/app/store.ts
@@ -0,0 +1,42 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { mmkvReduxStorage } from '@core/storage/mmkv';
+import authReducer from '@modules/auth/slices/authSlice';
+import taskReducer from '@modules/tasks/slices/taskSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage: mmkvReduxStorage,
+  whitelist: ['auth'],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  tasks: taskReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
